a6cec95a60b9e795c7411e43ecfc243de0336694
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2023-12-22 13:22:43 +0200

    Fixed syntax error

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ const App = () => {
     setLoading(true);
     setIsOpen(true);
 
-    // This is the HTTP request that is sent to OpenAI API.
     const options = {
       method: 'POST',
       headers: {
